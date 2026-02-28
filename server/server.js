5ca9fd6239fa325567760beb229b8994292c55d6
Xalvion <codewithxal@gmail.com>
2026-02-28 15:46:30 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -18,7 +18,8 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL,
+      "http://localhost:5173"],
     credentials: true,
   }),
 );
